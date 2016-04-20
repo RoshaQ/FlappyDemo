24afa939e114b5bb4bf9963cd5fb5fa78f090e13
RoshaQ <mikolaj.roshaq@gmail.com>
2016-04-20 16:22:14 +0200

    odcinki od 8 do 10

--- core/src/com/roshaq/flappy/sprites/Bird.java
+++ core/src/com/roshaq/flappy/sprites/Bird.java
@@ -1,6 +1,7 @@
 package com.roshaq.flappy.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
 /**
@@ -8,15 +9,17 @@ import com.badlogic.gdx.math.Vector3;
  */
 public class Bird {
     private static final int GRAVITY = -15;
+    private static final int MOVEMENT = 100;
     private Vector3 position; // wektor ten przeochowuje pozycje X Y i Z my używamy tylko X i Y
     private Vector3 velocity;
     private Texture bird;
+    private Rectangle bounds;
 
     public Bird(int x,int y){
         position = new Vector3(x,y,0);
         velocity = new Vector3(0,0,0);
         bird = new Texture("bird.png");
-
+        bounds = new Rectangle(x,y,bird.getWidth(),bird.getHeight());
     }
 
     public void update(float dt)
@@ -26,11 +29,12 @@ public class Bird {
         }
         velocity.scl(dt); // skaluje wektor za pomocą skalara
 
-        position.add(0, velocity.y, 0);
+        position.add(MOVEMENT * dt, velocity.y, 0);
         if(position.y<0) {
             position.y = 0;
         }
         velocity.scl(1/dt);
+        bounds.setPosition(position.x, position.y);
 
     }
 
@@ -43,5 +47,8 @@ public class Bird {
     }
     public void jump(){
         velocity.y=250;
+    }
+    public Rectangle getBounds(){
+        return bounds;
     }
 }
